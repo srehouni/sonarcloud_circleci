14c1009f015319f1e2f4a56a3b4613bdbd408d4b
Said Rehouni <saidrehouni@Saids-MacBook-Pro-2.local>
2023-07-05 10:43:25 +0200

    init project

---
+++ Sonarcloud/Sonarcloud/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sonarcloud
+//
+//  Created by Said Rehouni on 5/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sonarcloud/Sonarcloud/SonarcloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarcloudApp.swift
+//  Sonarcloud
+//
+//  Created by Said Rehouni on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SonarcloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
